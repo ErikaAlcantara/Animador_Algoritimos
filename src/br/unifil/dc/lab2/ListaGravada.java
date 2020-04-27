6ce7a05a049840711d53944268bb96855fb42dee
guiulerme <gui.rodrigues@edu.unifil.br>
2020-04-26 21:22:13 -0300

    Alterações listagravada e tocador

--- src/br/unifil/dc/lab2/ListaGravada.java
+++ src/br/unifil/dc/lab2/ListaGravada.java
@@ -1,16 +1,19 @@
 package br.unifil.dc.lab2;
 
+import java.awt.BasicStroke;
 import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Color;
-import java.util.Arrays;
+import java.awt.FontMetrics;
+import java.awt.Stroke;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Write a description of class ListaGravada here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class ListaGravada implements Transparencia
@@ -23,21 +26,102 @@ public class ListaGravada implements Transparencia
         this.nome = nome;
         this.coresIndices = coresIndices;
     }
-    
-    
+
+
     public void pintar(Graphics2D pincel, JPanel contexto) {
+        int X = contexto.getWidth();
+        int Y = contexto.getHeight();
+
+        acharMaiorElementos(lista);
+        compararElementos(lista, maiorElemento);
+        definirTamanhos(lista, X, Y);
+        desenharGraficos(pincel);
         Dimension dim = contexto.getSize();
-        System.out.println(lista);
-
-
-
-        // throw new RuntimeException("Funcionalidade ainda não implementada pelo aluno");
-
-
     }
-    
-    
+
+    private void compararElementos(List<Integer> valores, int maiorElemento) {
+        listaProporcoes = new ArrayList<>();
+        for(int i = 0; i < lista.size(); i++) {
+            float listTemp = lista.get(i);
+            float temp = listTemp / maiorElemento;
+            listaProporcoes.add(i, temp);
+        }
+    }
+
+    private void acharMaiorElementos(List<Integer> lista) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i) > maiorElemento) {
+                maiorElemento = lista.get(i);
+            }
+        }
+    }
+
+    public void definirTamanhos(List<Integer> lista, int X, int Y) {
+        double porcentagemLargura = 60;
+        double porcentagemDistancia = 41.7;
+        double porcentagemMargem = 16.7;
+
+        porcentagemLargura = porcentagemLargura / lista.size();
+        porcentagemDistancia = porcentagemDistancia / lista.size();
+        porcentagemMargem = porcentagemMargem / lista.size();
+
+        largBlocos = ((X / 100) * porcentagemLargura);
+        espacBlocos = ((X / 100) * porcentagemDistancia);
+        margem = ((X / 100) * porcentagemMargem);
+
+        maiorBloco = Y / (Y / (float) (Y / 2));
+        margemSuperior = Y / (Y / (float) (Y / 4 ));
+    }
+
+    private void desenharGraficos(Graphics2D pincel) {
+        int xAux = (int) margem;
+        for (int i = 0; i < lista.size(); i++) {
+
+            if (coresIndices.get(i) == null) {
+                pincel.setColor(Color.cyan);
+            } else {
+                pincel.setColor(coresIndices.get(i));
+            }
+
+            double alturaI = maiorBloco * listaProporcoes.get(i);
+            double eixoY = margemSuperior + (maiorBloco - alturaI);
+
+            pincel.fillRect((int) xAux, (int) eixoY,
+                    (int) largBlocos, (int) alturaI);
+
+
+            pincel.setStroke(bs);
+            pincel.setColor(Color.black);
+
+            pincel.drawRect((int) xAux, (int) eixoY,
+                    (int) largBlocos, (int) alturaI);
+
+            desenharString(pincel, i, xAux, eixoY, alturaI);
+
+
+            xAux += (int) largBlocos + (int) espacBlocos;
+        }
+    }
+
+    private void desenharString(Graphics2D pincel, int i, int xAux, double eixoY, double alturaI) {
+        nome = lista.get(i).toString();
+        FontMetrics fm = pincel.getFontMetrics();
+
+        double xString = xAux + (largBlocos / 2) - (fm.stringWidth(nome) / 2);
+        double yString = (eixoY + alturaI + 1) + fm.getHeight();
+        pincel.drawString(nome, (int) xString, (int) yString);
+    }
+
+
     private List<Integer> lista;
+    private List<Float> listaProporcoes;
     private List<Color> coresIndices;
     private String nome;
+    private int maiorElemento;
+    private BasicStroke bs = new BasicStroke(3);
+    private double largBlocos;
+    private double espacBlocos;
+    private double margem;
+    private double maiorBloco;
+    private double margemSuperior;
 }
--- src/br/unifil/dc/lab2/Tocador.java
+++ src/br/unifil/dc/lab2/Tocador.java
@@ -2,15 +2,13 @@ package br.unifil.dc.lab2;
 
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Color;
+import java.awt.*;
 import java.util.ListIterator;
 
 /**
  * Write a description of class Tocador here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Tocador extends JPanel {
@@ -18,55 +16,68 @@ public class Tocador extends JPanel {
     public Tocador(ListIterator<Transparencia> quadrosFilme) {
         setBackground(Color.WHITE);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        
+
         carregarFilme(quadrosFilme);
     }
+
     public Tocador() {
         this(null);
     }
-    
+
     public void carregarFilme(ListIterator<Transparencia> quadrosFilme) {
         this.quadrosFilme = quadrosFilme;
         this.quadroAtual = null;
         numQuadro = 0;
     }
-    
+
     public void avancarFilme() {
         if (quadrosFilme.hasNext()) {
             quadroAtual = quadrosFilme.next();
             numQuadro++;
         }
     }
-    
+
     public void voltarFilme() {
         if (quadrosFilme.hasPrevious()) {
             quadroAtual = quadrosFilme.previous();
             numQuadro--;
         }
     }
-    
+
     public void rebobinarFilme() {
         while (quadrosFilme.hasPrevious()) {
             quadroAtual = quadrosFilme.previous();
             numQuadro--;
         }
     }
-    
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D pincel = (Graphics2D) g;
-        
+
         if (quadroAtual != null) {
             quadroAtual.pintar(pincel, this);
-        } /*else {
-            // ESCREVER NO MEIO DA TELA "O Filme ainda não iniciou."
-            throw new RuntimeException("O aluno ainda não implementou essa funcionalidade.");
-        }*/
-        
-        // ESCREVER NO CANTO INFERIOR DIREITO DA TELA "Quadro 'numQuadro'"
-        //throw new RuntimeException("O aluno ainda não implementou essa funcionalidade.");
+        } else {
+            String texto = "O filme ainda não iniciou.";
+            FontMetrics fm = pincel.getFontMetrics();
+
+            Dimension dim = getSize();
+
+            double xString = (dim.width / 2) - (fm.stringWidth(texto) / 2);
+            double yString = (dim.height / 2) - (fm.getHeight() / 2);
+            pincel.drawString(texto, (int) xString, (int) yString);
+        }
+
+        String texto = "Quadro 'numQuadro' ";
+        FontMetrics fm = pincel.getFontMetrics();
+
+        Dimension dim = getSize();
+
+        double xString = dim.width - fm.stringWidth(texto) - 3;
+        double yString = dim.height - fm.getHeight() + 8;
+        pincel.drawString(texto, (int) xString, (int) yString);
     }
-    
+
     private int numQuadro = 0;
     private Transparencia quadroAtual = null;
     private ListIterator<Transparencia> quadrosFilme = null;
